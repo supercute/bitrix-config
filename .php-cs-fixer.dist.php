ae6a75189e70ca7bdc01a5a5291b727b5068e637
Danil Shumskikh <d@shumskikh.ru>
2026-07-04 13:38:56 +0300

    style: change phpcsfixer configuration

--- .php-cs-fixer.dist.php
+++ .php-cs-fixer.dist.php
@@ -2,11 +2,30 @@
 
 $finder = PhpCsFixer\Finder::create()
     ->in(__DIR__ . '/src')
-    ->in(__DIR__ . '/tests');
+    ->in(__DIR__ . '/tests')
+    ->exclude('fixtures');
 
 return (new PhpCsFixer\Config())
-    ->setRiskyAllowed(false)
+    ->setRiskyAllowed(true)
     ->setRules([
         '@PER-CS' => true,
+
+        'declare_strict_types' => true,
+        'no_unused_imports' => true,
+
+        // Базово только use, расширяем для всего
+        'no_extra_blank_lines' => true,
+
+        'single_class_element_per_statement' => [
+            'elements' => ['property', 'const'],
+        ],
+
+        'class_attributes_separation' => [
+            'elements' => [
+                'method' => 'one',
+                'property' => 'one',
+                'const' => 'one',
+            ],
+        ],
     ])
     ->setFinder($finder);
